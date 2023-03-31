a44dbbc7314ebec3e16a5a434448e5f608fb8624
José <ricardomafrajose@gmail.com>
2023-03-31 13:15:51 -0300

    main page ok

--- AlugameCar/src/app/components/homepage/homepage.component.ts
+++ AlugameCar/src/app/components/homepage/homepage.component.ts
@@ -1,18 +1,25 @@
 import { PoMenuItem } from '@po-ui/ng-components';
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
+apply(arg0: any) {
+throw new Error('Method not implemented.');
+}
   collapse : boolean=true;
 
 
   readonly menus: Array<PoMenuItem> = [
     { label: ' ', action: this.onClick.bind(this), shortLabel:' ',icon: "po-icon-align-justify"},
   ];
+formAirfare: any;
+  ticketClassOptions: any[] = [];
+  accompanyNumber: any[] = [];
 
   private onClick() {
     this.collapse = !this.collapse;
